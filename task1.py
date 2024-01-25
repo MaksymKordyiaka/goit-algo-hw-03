06082187f1cd4ecae4cb2a54d3dadb52acda383b
Maksym <maxkord2003@gmail.com>
2024-01-25 13:52:01 +0200

    third commit

--- task1.py
+++ task1.py
@@ -1,1 +1,19 @@
-print('Home')
+'''
+Створіть функцію get_days_from_today(date), яка розраховує 
+кількість днів між заданою датою і поточною датою.
+'''
+from datetime import datetime
+
+def get_days_from_today(date):
+    try:
+        date = date.replace(' ', '-')
+        date_not_str = datetime.strptime(date, '%Y-%m-%d')
+        today_date = datetime.today()
+        difference_date = today_date - date_not_str
+        difference_days = difference_date.days
+        print(f'Різниця у днях між {date} та сьогодні: {difference_days} днів')
+    except ValueError:
+        print(f'Неправильний формат дати')
+
+input_date = input('Введіть будь-яку дату у форматі YYYY MM DD: ')    
+get_days_from_today(input_date)
